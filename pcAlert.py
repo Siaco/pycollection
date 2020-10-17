cd6ebb5fe81cee419f3ce3ad9857909069c54ec0
Blackplague83 <siaco83@gmail.com>
2020-10-17 16:18:20 +0200

    script to get alert from PC for a particular city

---
+++ pcAlert.py
@@ -0,0 +1,28 @@
+import urllib.request
+import json
+urlT = 'http://www.protezionecivilepop.tk/allerte?citta=060038&rischio=temporali&allerta=verde&giorno=domani&formato=json'
+urlI = 'http://www.protezionecivilepop.tk/allerte?citta=060038&rischio=idraulico&allerta=verde&giorno=domani&formato=json'
+urlG = 'http://www.protezionecivilepop.tk/allerte?citta=060038&rischio=idrogeologico&allerta=verde&giorno=domani&formato=json'
+
+reqT = urllib.request.Request(urlT)
+reqI = urllib.request.Request(urlI)
+reqG = urllib.request.Request(urlG)
+
+##parsing response
+r = urllib.request.urlopen(reqI).read()
+previsioneT = json.loads(r.decode('utf-8'))
+r = urllib.request.urlopen(reqT).read()
+previsioneI = json.loads(r.decode('utf-8'))
+r = urllib.request.urlopen(reqG).read()
+previsioneG = json.loads(r.decode('utf-8'))
+
+##parsing json
+print("Livello allerta comune di Frosinone:")
+print("Rischio Temporali:", previsioneT['previsione']['alert'])
+print("Rischio Idraulico:", previsioneI['previsione']['alert'])
+print("Rischio Idrogeologico:", previsioneG['previsione']['alert'])
+print("----")
+if previsioneT['previsione']['alert'] =='VERDE':
+    if previsioneI['previsione']['alert']=='VERDE':
+        if previsioneG['previsione']['alert']=='VERDE':
+            print("TUTTO BENE")
